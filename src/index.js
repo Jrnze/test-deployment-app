257d3db00d8c04f87b97a6953f8d594f82cce176
Jose Lorenzo Andicoy <jose.andicoy@awsys-i.com>
2022-11-09 09:49:03 +0800

    Add existing project files prior to the push to GitHub.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/Button';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+      <App />
   </React.StrictMode>
 );
 
